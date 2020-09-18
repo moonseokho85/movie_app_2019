7dd8e7d617c2ae2d9343c4b271ff52b4a8478737
Logan Moon <mseokho7733@gmail.com>
2020-09-18 12:26:33 +0900

    Updates

---
+++ precache-manifest.7f9f0c5413d0407eb65de6fc79c55297.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc58208f383486fd579cddd6ec29f10d",
+    "url": "/movie_app_2019.git/index.html"
+  },
+  {
+    "revision": "2c79db16ec1ba29a0f43",
+    "url": "/movie_app_2019.git/static/css/main.100b97f5.chunk.css"
+  },
+  {
+    "revision": "4d7c806d0e258b226914",
+    "url": "/movie_app_2019.git/static/js/2.00a09d16.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/movie_app_2019.git/static/js/2.00a09d16.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2c79db16ec1ba29a0f43",
+    "url": "/movie_app_2019.git/static/js/main.af076232.chunk.js"
+  },
+  {
+    "revision": "8f8aeb07e1ed67b2046a",
+    "url": "/movie_app_2019.git/static/js/runtime-main.78db7209.js"
+  }
+]);
